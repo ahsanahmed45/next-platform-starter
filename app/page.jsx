4862b3b4eb1cfeb6313b3a4e6cf0a4955a7b0d4f
ahsanahmed45 <168276246+ahsanahmed45@users.noreply.github.com>
2024-04-28 09:53:32 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,40 @@
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa
+Performance Management - Emirates Airlines
+Student Names:
+Aisha
+Mohammed Salman
+Sultan Taher
+Sultan Alhajri
+Introduction: Emirates Airlines, headquartered in Dubai, is one of the global carriers that is very popular for its highly luxurious style of flying and superior-to-the-rest services.
+
+Mission: Emirates Airlines aims to become the global leader in outstanding air travel by providing unparalleled quality, safety, and comfort to all its passengers.
+
+Vision: Emirates aims to compete with its global airline competitors in luxury, innovation, and perfection.
+
+Goals: This organization tries to create new standards by innovating, optimizing operations, and prioritizing consumer needs.
+
+Objectives of the Performance Management:
+Enhancing Employee Performance.
+Aligning Individual and Corporate Goals, Through setting SMART goals.
+Identifying the Training and Development Needs of its employees.
+The process is thus assisting in rewarding merit, granting employee incentives, and granting opportunities for career advancements.
+Flow Chart of the Performance Management Process
+Goal Setting
+Performance Monitoring
+Performance Evaluation
+Development Planning
+Small Bonuses and Appreciation
+HR Department Overview
+The Human Resources (HR) department at Emirates Airlines is the key contributor to the company's capability to attract, grow and retain the top performing employees.
+
+The roles provided by the HR department are:
+
+Recruitment and Talent Acquisition
+Training and Development
+Performance Management
+Employee Relations
+Compensation and Benefits
+HR Policy Development
+References
+Alsuwaidi, M., Alshurideh, M., Al Kurdi, B., & Salloum, S. A. (2021). Performance appraisal on employees’ motivation: A comprehensive analysis. In Proceedings of the International Conference on Advanced Intelligent Systems and Informatics 2020 (pp. 681-693). Springer International Publishing.
+
+Thank You

51211bb44e6cf678d1e5d76c1517c590fda67084
ahsanahmed45 <168276246+ahsanahmed45@users.noreply.github.com>
2024-04-28 12:49:02 +0500

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
    
-               
+                <h2 className="mb-0">Emirates - Fly Better</h2>
                
 
 const navItems = [
-    { linkText: 'Emirates - Fly Better',  '/' }
+    { linkText: '<h2 className="mb-0">Emirates - Fly Better</h2>',  '/' }
     
 ];
 

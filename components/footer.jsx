2dfd0152335b1de88b2cf806e2d35fbbf46dfea8
ahsanahmed45 <168276246+ahsanahmed45@users.noreply.github.com>
2024-04-28 12:22:22 +0500

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,12 +4,12 @@ export function Footer() {
     return (
         <footer className="pt-16 pb-12 sm:pt-24 sm:pb-16">
             <p className="text-sm">
-             {!!ctx && (
+            
                 <section className="flex flex-col gap-4">
                   
                     <img src="https://image.similarpng.com/very-thumbnail/2020/04/UAE-united-arab-emirates-flag-png.png" />
                 </section>
-            )}
+           
             </p>
         </footer>
     );
